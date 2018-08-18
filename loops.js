ff81b7295f33aaf9ae792f0537603a3cb367cf01
Mark Ehler <ehlerm42@gmail.com>
2018-08-18 02:10:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-
+ /* 
 function forLoop(crackers){
   for (var i = 1; i < 25; i++) {
     if (i === 1) {
@@ -12,7 +12,7 @@ function forLoop(crackers){
   return crackers;
 }
 
- /*     
+    */
 function forLoop(array) {
   
   for (let i = 0; i < 25; i++) {
@@ -26,7 +26,7 @@ function forLoop(array) {
   return array
 }
 
-*/
+
 function whileLoop(n) {
   while (n > 0) {
     console.log(--n);
